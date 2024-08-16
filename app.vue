6346504066087e1edc103fd94f1c33951ab437f6
Kheuval <hadrien.hartstein@gmail.com>
2024-08-16 19:40:06 +0200

    first commit

---
+++ app.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div>
+</template>
+
+<script setup lang="ts">
+const languageNames = new Intl.DisplayNames("en", { type: "language" });
+const regionNames = new Intl.DisplayNames("en", { type: "region" });
+const currencyNames = new Intl.DisplayNames("en", { type: "currency" });
+
+console.log(languageNames.of("fr"));
+console.log(regionNames.of("fr"));
+console.log(currencyNames.of("eur"));
+</script>
